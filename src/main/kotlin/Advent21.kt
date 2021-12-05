2100324fa6c78695778903e25c57f3570dba4082
Moustafa Abdel Fattah <ripple_92@live.com>
2021-12-05 23:18:50 +0200

    2021[5]

--- src/main/kotlin/Advent21.kt
+++ src/main/kotlin/Advent21.kt
@@ -1,6 +1,9 @@
 import java.nio.charset.Charset
 import java.nio.file.Paths
 import kotlin.io.path.readLines
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun readLines(path: String): List<String> =
     {}::class.java.getResource(path)?.let { Paths.get(it.toURI()).readLines(Charset.defaultCharset()) } ?: emptyList()
@@ -132,8 +135,46 @@ fun puzzle4b(): Int {
     }
 }
 
+fun puzzle5a(): Int {
+    return readLines("advent21/puzzle5-input.txt")
+        .map { it.split("->").flatMap { p -> p.trim().split(",") } }
+        .map { (x1, y1, x2, y2) -> Pair(min(x1.toInt(), x2.toInt())..max(x1.toInt(), x2.toInt()), min(y1.toInt(), y2.toInt())..max(y1.toInt(), y2.toInt())) }
+        .filter { (xs, ys) -> xs.first == xs.last || ys.first == ys.last }
+        .flatMap { (xs, ys) -> xs.flatMap { x -> ys.map { y -> Pair(x, y) } } }
+        .groupBy { it }
+        .filter { it.value.size > 1 }
+        .count()
+}
+
+fun puzzle5b(): Int {
+
+    return readLines("advent21/puzzle5-input.txt")
+        .map { it.split("->").flatMap { p -> p.trim().split(",").map { it.toInt() } } }
+        .filter { (x1, y1, x2, y2) -> x1 == x2 || y1 == y2 || abs(y2 - y1) == abs(x2 - x1)  }
+        .fold(mutableMapOf<Pair<Int, Int>, Int>()) { acc, (x1, y1, x2, y2) ->
+            if(x1 == x2 || y1 == y2){
+                (min(x1,x2)..max(x1,x2)).forEach { x ->
+                    (min(y1,y2)..max(y1,y2)).forEach { y ->
+                        val point = Pair(x,y)
+                        acc[point] = acc.getOrPut(point) {0} + 1
+                    }
+                }
+            } else {
+                val slope = (y2 - y1)/(x2 - x1)
+                val b = (y1 - x1 * slope)
+                (min(x1,x2)..max(x1,x2)).forEach { x ->
+                    val point = Pair(x, b + x * slope)
+                    acc[point] = acc.getOrPut(point) {0} + 1
+                }
+            }
+            acc
+        }
+        .filterValues { it > 1 }
+        .count()
+}
+
 
 
 fun main() {
-    println(puzzle4b())
+    println(puzzle5b())
 }
